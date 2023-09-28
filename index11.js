5cf7700e74a48b06ccbec53ffab5d1df88288eeb
konstantinkhamilov <kkhamilo82@gmail.comm>
2023-09-28 22:51:05 +0300

    final1

--- index11.js
+++ index11.js
@@ -1,24 +1,52 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/";
 
+// Массив товаров
+let products = [
+    { id: 1, name: 'Product 1', price: 100 },
+    { id: 2, name: 'Product 2', price: 200 },
+    { id: 3, name: 'Product 3', price: 300 },
+    // ...
+];
+
+// Middleware для валидации и приведения типов
+function validateAndCast(req, res, next) {
+    if (req.params.id) {
+        req.params.id = Number(req.params.id);
+    }
+    if (req.query.price) {
+        req.query.price = Number(req.query.price);
+    }
+    next();
+}
+
+app.use(cors()); // Добавьте эту строку для включения CORS
 app.use(express.json()); // Для парсинга JSON в запросах
+app.use(validateAndCast);
 
-// Route для добавления нового товара
-app.post('/products', (req, res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("mydb");
-        var newProduct = req.body;
-        dbo.collection("products").insertOne(newProduct, function(err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-        });
-    });
-    res.send('Product added!');
+// Route для получения всех товаров
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
+// Route для получения товара по id
+app.get('/products/:id', (req, res) => {
+    const product = products.find(p => p.id === req.params.id);
+    res.json(product);
+});
+
+// Route для фильтрации товаров по названию и цене
+app.get('/products/filter', (req, res) => {
+    let filteredProducts = products;
+    if (req.query.name) {
+        filteredProducts = filteredProducts.filter(p => p.name.includes(req.query.name));
+    }
+    if (req.query.price) {
+        filteredProducts = filteredProducts.filter(p => p.price <= req.query.price);
+    }
+    res.json(filteredProducts);
 });
 
 app.listen(port, () => {
