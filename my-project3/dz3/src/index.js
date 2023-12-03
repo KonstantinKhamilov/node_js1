05bc66d8a77859b11d69fdc4a49d9888be101970
konstantinkhamilov <kkhamilo82@gmail.comm>
2023-12-03 20:26:56 +0200

    dz6

--- my-project3/dz3/src/index.js
+++ my-project3/dz3/src/index.js
@@ -4,11 +4,94 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Instruction from './Instruction';
 
+class Wrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showNavbar: true, showContent1: false };
+  }
+
+  toggleNavbar = () => {
+    this.setState({ showNavbar: !this.state.showNavbar });
+  }
+
+  toggleContent1 = () => {
+    this.setState({ showContent1: !this.state.showContent1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleNavbar}>Toggle Navbar</button>
+        {this.state.showNavbar && <Navbar />}
+        <button onClick={this.toggleContent1}>Toggle Content1</button>
+        {this.state.showContent1 && <Content1 />}
+      </div>
+    );
+  }
+}
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: 'Some data' };
+  }
+
+  componentDidMount() {
+    console.log('Navbar mounted');
+  }
+
+  componentDidUpdate() {
+    console.log('Navbar updated');
+  }
+
+  componentWillUnmount() {
+    console.log('Navbar will unmount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should Navbar update?');
+    return true;
+  }
+
+  render() {
+    return <div>{this.state.data}</div>;
+  }
+}
+
+class Content1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: 'Some other data' };
+  }
+
+  componentDidMount() {
+    console.log('Content1 mounted');
+  }
+
+  componentDidUpdate() {
+    console.log('Content1 updated');
+  }
+
+  componentWillUnmount() {
+    console.log('Content1 will unmount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should Content1 update?');
+    return true;
+  }
+
+  render() {
+    return <div>{this.state.data}</div>;
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Instruction />
+        <Wrapper />
       </div>
     );
   }
